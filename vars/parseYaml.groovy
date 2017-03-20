15e1972534b67870670a3953d956a7f024e3430f
Simon Leonhardt <aukaost@gmail.com>
2017-03-20 20:40:12 +0100

    return parsed YAML object

---
+++ vars/parseYaml.groovy
@@ -0,0 +1,14 @@
+#!/usr/bin/groovy
+
+@Grab('org.yaml:snakeyaml:1.18')
+import org.yaml.snakeyaml.Yaml
+
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    Yaml parser = new Yaml()
+    return parser.load((config.filePath as File).text)
+}
